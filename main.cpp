01bc3d376f0559cdb15fb6b16aabfae3e162b5d5
Happytyagi11 <Happy7470@conestogac.on.ca>
2025-08-16 16:11:40 -0400

    Update main.cpp
    try ConsoleIO.c function

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include "Archive.h"
 #include "Compression.h"
+#include "ConsoleIO.h"
 
 void printBinary(const char data) {
   for (int i = 7; i >= 0; i--) {
@@ -16,10 +17,25 @@ void printBinary(const char data) {
 }
 
 int main() {
-  createArchive("test_archive");
-  FILE* archivePtr = openArchive("test_archive");
-  addFile(archivePtr, "Galaxy.dll");
-  closeArchive(archivePtr);
+
+  // creade root note and add folders and files to it
+PathNode* root = createNode("ROOT", false);
+
+addPath(root, "src/main.c");
+addPath(root, "src/utils/helpers.c");
+addPath(root, "src/utils/io.c");
+addPath(root, "include/main.h");
+addPath(root, "README.md");
+addPath(root, "docs/usage.txt");
+
+printTree(root);
+freeTree(root);
+
+  
+createArchive("test_archive");
+FILE* archivePtr = openArchive("test_archive");
+addFile(archivePtr, "Galaxy.dll");
+closeArchive(archivePtr);
   
 
   return 0;
